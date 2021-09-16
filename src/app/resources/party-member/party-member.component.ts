7e82f73b2ae47934e334438c739bfaeb21de3de3
Scott <scottin3d@gmail.com>
2021-09-16 15:22:10 -0700

    injecting party members

--- src/app/resources/party-member/party-member.component.ts
+++ src/app/resources/party-member/party-member.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-party-member',
@@ -7,6 +7,8 @@ import { Component, Input } from '@angular/core';
 })
 export class PartyMemberComponent{
   @Input() member: PartyMember|null = null;
+  @Output() edit = new EventEmitter<PartyMember>();
+
 }
 
 export interface PartyMember {
